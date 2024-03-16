81e82ab801360973f7de48af2fdcdab72cb5fd6b
Nathan3-14 <nathan.watson.172@gmail.com>
2024-03-16 13:50:47 +0000

    Changed empty default

--- defaults/default.py
+++ defaults/default.py
@@ -2,7 +2,7 @@ from defaults.parent import Block
 
 
 class A(Block):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("A")
     
     def tick(self) -> None:
@@ -11,8 +11,13 @@ class A(Block):
 class B(Block):
     def __init__(self):
         super().__init__("B")
+    
+    def tick(self) -> None:
         self.position[0] -= 1
 
-class Empty:
+class Empty(Block):
+    def __init__(self) -> None:
+        pass
+
     def __str__(self) -> str:
         return " "
